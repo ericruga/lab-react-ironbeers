553d7632f7e25b6291fe86b4414ef8dfd5cdae00
ericruga <ericruga@users.noreply.github.com>
2022-07-16 10:33:59 +0200

    Changes on Iteration 1

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,17 +8,17 @@ function Homepage() {
     <div>
       <h1>HOME PAGE</h1>
       <Link to="/beers">
-      <img src={Beers} alt="beers" className="beers" />
+      <img src={Beers} alt="beers" className="beers" height="600px"/>
         <h3>Beers</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam metus sapien, consequat at aliquam vel, aliquam id erat. Proin scelerisque porta porttitor. Donec feugiat enim lobortis sem tristique sollicitudin. Morbi eu augue sed leo scelerisque luctus nec cursus eros. Ut quis congue felis, tincidunt rhoncus lorem. Curabitur at elit lectus. Aenean tincidunt scelerisque pulvinar. Aenean quis odio vestibulum quam gravida convallis eget at sapien. In vitae massa massa. Cras volutpat condimentum magna, sed posuere mauris euismod a. Nunc eleifend tincidunt elit vitae dictum. Vivamus eu odio vel dolor tincidunt posuere at non ipsum.</p>
       </Link>
       <Link to="/random-beer">
-      <img src={RandomBeer} alt="randombeer" className="beers" />
+      <img src={RandomBeer} alt="randombeer" className="beers" height="600px"/>
         <h3>Random Beer</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam metus sapien, consequat at aliquam vel, aliquam id erat. Proin scelerisque porta porttitor. Donec feugiat enim lobortis sem tristique sollicitudin. Morbi eu augue sed leo scelerisque luctus nec cursus eros. Ut quis congue felis, tincidunt rhoncus lorem. Curabitur at elit lectus. Aenean tincidunt scelerisque pulvinar. Aenean quis odio vestibulum quam gravida convallis eget at sapien. In vitae massa massa. Cras volutpat condimentum magna, sed posuere mauris euismod a. Nunc eleifend tincidunt elit vitae dictum. Vivamus eu odio vel dolor tincidunt posuere at non ipsum.</p>
       </Link>
       <Link to="/new-beer">
-      <img src={NewBeer} alt="newbeer" className="beers" />
+      <img src={NewBeer} alt="newbeer" className="beers" height="600px"/>
         <h3>New Beer</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam metus sapien, consequat at aliquam vel, aliquam id erat. Proin scelerisque porta porttitor. Donec feugiat enim lobortis sem tristique sollicitudin. Morbi eu augue sed leo scelerisque luctus nec cursus eros. Ut quis congue felis, tincidunt rhoncus lorem. Curabitur at elit lectus. Aenean tincidunt scelerisque pulvinar. Aenean quis odio vestibulum quam gravida convallis eget at sapien. In vitae massa massa. Cras volutpat condimentum magna, sed posuere mauris euismod a. Nunc eleifend tincidunt elit vitae dictum. Vivamus eu odio vel dolor tincidunt posuere at non ipsum.</p>
       </Link>
